6d4916766164a2c6981af7bd8ad2ba3bf6ed41be
Milan Stojanovic <milanstoj94@gmail.com>
2025-12-09 15:47:44 -0600

    Initial Commit

---
+++ run-it-back/run_it_backApp.swift
@@ -0,0 +1,17 @@
+//
+//  run_it_backApp.swift
+//  run-it-back
+//
+//  Created by Milan Stojanovic on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct run_it_backApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
